f078cd00b2f107f6a28775707f94ede6db6351d6
Chaudhary, Anoop <anoop.chaudhary@thermofisher.com>
2018-06-22 10:51:35 +0530

    Non Reactive Js

---
+++ ReactivityInJs.js
@@ -0,0 +1,6 @@
+let price = 5
+let quantity = 2
+let total = price * quantity
+price = 20
+console.log(`total is ${total}`) //gonna print 10, In Vue we want total to get updated 
+                                // whenever price or quantity get updated.
