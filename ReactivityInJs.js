cdc268c52b26fd719d60ddbf9629c412351c762b
Chaudhary, Anoop <anoop.chaudhary@thermofisher.com>
2018-06-22 16:00:14 +0530

    Getter and setter for price property

--- ReactivityInJs.js
+++ ReactivityInJs.js
@@ -38,8 +38,41 @@ let target =  () => { total = price * quantity }
 dep.depend() // Add this target to our subscribers
 target() // Run it to get the total
 
-console.log(total) // => 10 >.. The right number
+// console.log(total) // => 10 >.. The right number
+// price = 20
+// console.log(total) // => 10 .. No longer the right number
+// dep.notify() //Run the subscribers
+// console.log(total) // => 40 .. Now the right number
+
+function watcher(myFunc) {
+    target = myFunc // Set as the active target
+    dep.depend() // Add the active target as a dependency
+    target() // Call the target
+    target = null // Reset the target
+}
+
+watcher(() => {
+    total = price * quantity
+})
+
 price = 20
-console.log(total) // => 10 .. No longer the right number
-dep.notify() //Run the subscribers
-console.log(total) // => 40 .. Now the right number
+console.log(total)
+dep.notify()      
+console.log(total) 
+
+let data = { price: 5, quantity: 2}
+let internalValue = data.price // Our initial value
+
+Object.defineProperty(data, 'price', {//For just the price property
+    get() {
+        console.log(`Getting price: ${internalValue}`)
+        return internalValue
+    },
+    set(newVal) {
+        console.log(`Setting price to: ${newVal}`)
+        internalValue = newVal
+    }
+})
+
+total = data.price * data.quantity
+data.price = 20 // This calls set()
