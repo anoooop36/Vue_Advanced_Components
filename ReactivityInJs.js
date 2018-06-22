5a215b0fbab8927dca61568eca43bf5bcf5f26a6
Chaudhary, Anoop <anoop.chaudhary@thermofisher.com>
2018-06-22 11:09:06 +0530

    Simple Reactivity in Js using functions and storage

--- ReactivityInJs.js
+++ ReactivityInJs.js
@@ -1,6 +1,24 @@
 let price = 5
 let quantity = 2
-let total = price * quantity
+let total = 0
+let target = null
+let storage = []
+
+target = function () {
+    total = price * quantity
+}
+
+//Remember this in case we want to run it later
+function record() { // target = () => price*quantity
+    storage.push(target)
+} 
+
+function replay () { // on change run each of the functions from storage
+    storage.forEach( run => run())
+}
+target()
 price = 20
-console.log(`total is ${total}`) //gonna print 10, In Vue we want total to get updated 
-                                // whenever price or quantity get updated.
+console.log(total) // => 10
+record()
+replay()
+console.log(total) // => 40
